6c7f44465248bee5d505ce18a2ec7da7fccc2302
= <=>
2022-02-28 12:57:11 +0100

    Protocol types enum.

--- src/interfaces/radar.interface.ts
+++ src/interfaces/radar.interface.ts
@@ -1,6 +1,15 @@
 import { Scan } from './scan.interface';
 
+export enum ProtocolType {
+  closestEnemies = 'closest-enemies',
+  furthestEnemies = 'furthest-enemies',
+  assistAllies = 'assist-allies',
+  avoidCrossfire = 'avoid-crossfire',
+  prioritizeMech = 'prioritize-mech',
+  avoidMech = 'avoidMesh',
+}
+
 export interface Radar {
-  protocols: string[];
+  protocols: ProtocolType[];
   scan: Scan[];
 }
