afe7ecde5959c233611c649579799e011a565133
= <=>
2022-02-28 14:16:03 +0100

    Post method and service.

--- src/radar/radar.controller.ts
+++ src/radar/radar.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Coordinates } from '../interfaces/coordinates.interface';
+import { Radar } from '../interfaces/radar.interface';
 
 @Controller('radar')
-export class RadarController {}
+export class RadarController {
+  @Post()
+  receiveRadarPositions(@Body() radarPositions: Radar[]): Coordinates {
+    console.log(radarPositions);
+    const coordinates = { x: 0, y: 40 };
+    return coordinates;
+  }
+}
--- src/radar/radar.service.ts
+++ src/radar/radar.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Coordinates } from '../interfaces/coordinates.interface';
 
 @Injectable()
-export class RadarService {}
+export class RadarService {
+  calculateDistance({ x, y }: Coordinates): number {
+    const distance: number = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    return distance;
+  }
+}
